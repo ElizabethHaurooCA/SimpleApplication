a107cfc39a57e5e9ba9cce09b13cbd10b334da61
ElizabethHauroo <eliza-hauroo13@hotmail.com>
2023-03-09 15:16:08 +0000

    updated pipeline:secured with https

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ pm2 stop simple_app
 cd SimpleApplication/
 #install dependencies
 npm install
-#echo $PRIVATE_KEY > privatekey.pem
-#echo $SERVER > server.crt
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
 #start the app
 pm2 start ./bin/www --name simple_app 
